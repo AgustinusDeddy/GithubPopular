c84fc8e0fe03e74d5dd89edaaaafff702a688b22
agustinusdeddy <deddyarief1@gmail.com>
2018-01-06 20:11:09 +0800

    Display list of repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,92 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+function SelectLanguage (props){
+
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+        <ul className='languages'>
+        {/* <p>Selected Language : {this.state.selectedLanguage}</p> */}
+            {languages.map(function(lang){
+
+                //console.log('Down Here!',this);
+
+                return (
+                    <li 
+                        style={lang === props.selectedLanguage ? {color:'#d0021b'} : null}
+                        onClick={props.onSelect.bind(null, lang)}
+                        key={lang}
+                    >
+                        {lang}
+                    </li>
+                )
+            })}    
+        </ul>
+     ) 
+}
+
+function RepoGrid (props){
+    return (
+        <ul className='popular-list'>
+            {props.repos.map(function (repo, index){
+                return (
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index+1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li><a href = {repo.html_url}>{repo.name}</a></li>
+                            <li><b>@{repo.owner.login}</b></li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+// class SelectLanguage extends React.Component{
+//     render(){
+
+//         var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+//          return (
+//             <ul className='languages'>
+//             {/* <p>Selected Language : {this.state.selectedLanguage}</p> */}
+//                 {languages.map(function(lang){
+
+//                     //console.log('Down Here!',this);
+
+//                     return (
+//                         <li 
+//                             style={lang === this.props.selectedLanguage ? {color:'#d0021b'} : null}
+//                             onClick={this.props.onSelect.bind(null, lang)}
+//                             key={lang}
+//                         >
+//                             {lang}
+//                         </li>
+//                     )
+//                 }, this)}    
+//             </ul>
+//          )
+//     }
+// }
+
+SelectLanguage.propTypes = {
+    selectedLanguage : PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
+
+RepoGrid.propTypes = {
+    repos :PropTypes.array.isRequired
+}
 
 class Popular extends React.Component {
 
@@ -6,44 +94,54 @@ class Popular extends React.Component {
         super(props);
 
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    //lifecycle
+
+    componentDidMount(){
+        
+        this.updateLanguage(this.state.selectedLanguage);
+    }
+
     updateLanguage(lang){
         this.setState(function(){
             return {
-                selectedLanguage: lang
+                selectedLanguage: lang,
+                repos:null
             }
         });
+
+        api.fetchPopularRepos(lang)
+            .then(function(repos){
+                this.setState(function(){
+                    return{
+                        repos: repos
+                    }
+                })
+                console.log(repos);
+            }.bind(this));
     }
 
     render(){
-        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+        //var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
         //console.log('Up Here!',this);
 
         return (
-            
-            <ul className='languages'>
-                {/* <p>Selected Language : {this.state.selectedLanguage}</p> */}
-                {languages.map(function(lang){
-
-                    //console.log('Down Here!',this);
-
-                    return (
-                        <li 
-                            style={lang === this.state.selectedLanguage ? {color:'#d0021b'} : null}
-                            onClick={this.updateLanguage.bind(null, lang)}
-                            key={lang}
-                        >
-                            {lang}
-                        </li>
-                    )
-                }, this)}    
-            </ul>
+            <div>
+                <SelectLanguage 
+                    selectedLanguage ={this.state.selectedLanguage}
+                    onSelect={this.updateLanguage}
+                />
+                
+                {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos}/>}
+                
+            </div>
         )
     }
 }
