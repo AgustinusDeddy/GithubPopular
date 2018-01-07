080bf28b93d3c5e6e89b850c2741abf23b9b673a
agustinusdeddy <deddyarief1@gmail.com>
2018-01-08 04:39:00 +0800

    Customize loading

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 function SelectLanguage (props){
 
@@ -139,7 +140,7 @@ class Popular extends React.Component {
                     onSelect={this.updateLanguage}
                 />
                 
-                {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos}/>}
+                {!this.state.repos ? <Loading text='Querying Github' speed={50} color='green'/> : <RepoGrid repos={this.state.repos}/>}
                 
             </div>
         )
