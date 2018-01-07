5658705a2af873826775ffc096a2a751a0946a09
agustinusdeddy <deddyarief1@gmail.com>
2018-01-07 19:32:10 +0800

    Display battle result

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,25 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function PlayerPreview (props) {
-    return (
-        <div>
-            <div className='column'>
-                <img className='avatar' src={props.avatar} alt={'Avatar for ' + props.username}/>
-                <h2 className='username'>@{props.username}</h2>
-                <button className='reset' onClick={props.onReset.bind(null, props.id)}>Reset</button>
-            </div>
-        </div>
-    )
-}
-
-PlayerPreview.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    onReset: PropTypes.func.isRequired
-}
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component{
 
@@ -128,11 +110,26 @@ class Battle extends React.Component {
                 <div className='row'>
                     {!playerOneName && <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit} />}
 
-                    {playerOneImage !== null && <PlayerPreview avatar={playerOneImage} username={playerOneName} onReset={this.handleReset} id='playerOne' />}
+                    {playerOneImage !== null && 
+                        <PlayerPreview avatar={playerOneImage} username={playerOneName} 
+                            // onReset={this.handleReset} 
+                            id='playerOne'
+                        >
+                            {/* <button className='reset' onClick={props.onReset.bind(null, props.id)}>Reset</button> */}
+                            <button className='reset' onClick={this.handleReset.bind(null, 'playerOne')}>Reset</button>
+                        </PlayerPreview>
+                    }
 
                     {!playerTwoName && <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit} />}
 
-                    {playerTwoImage !== null && <PlayerPreview avatar={playerTwoImage} username={playerTwoName} onReset={this.handleReset} id='playerTwo' />}
+                    {playerTwoImage !== null && 
+                        <PlayerPreview avatar={playerTwoImage} username={playerTwoName} 
+                            // onReset={this.handleReset} 
+                            id='playerTwo' 
+                        >
+                            <button className='reset' onClick={this.handleReset.bind(null, 'playerTwo')}>Reset</button>
+                        </PlayerPreview>
+                    }
 
                 </div>
 
---
+++ app/components/PlayerPreview.js
@@ -0,0 +1,23 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+function PlayerPreview (props) {
+    return (
+        <div>
+            <div className='column'>
+                <img className='avatar' src={props.avatar} alt={'Avatar for ' + props.username}/>
+                <h2 className='username'>@{props.username}</h2>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+PlayerPreview.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+    // id: PropTypes.string.isRequired,
+    // onReset: PropTypes.func.isRequired
+}
+
+module.exports = PlayerPreview;
